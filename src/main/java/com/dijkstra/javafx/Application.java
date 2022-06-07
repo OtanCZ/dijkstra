c4e722354491cc39a74d48d901287fa2f2ce1728
Oťan <otto2004@seznam.cz>
2022-06-07 23:27:57 +0200

    zpáchací zpráva

--- src/main/java/com/dijkstra/javafx/Application.java
+++ src/main/java/com/dijkstra/javafx/Application.java
@@ -13,6 +13,7 @@ public class Application extends javafx.application.Application {
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Dijkstra's algorithm");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
--- src/main/java/com/dijkstra/javafx/Controller.java
+++ src/main/java/com/dijkstra/javafx/Controller.java
@@ -4,9 +4,7 @@ import com.dijkstra.algorithm.Algorithm;
 import com.dijkstra.algorithm.Node;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToolBar;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 
 public class Controller {
@@ -18,12 +16,16 @@ public class Controller {
     public TextField nodeName;
     public TextField infoText;
     public TextField pathWeight;
+    public ListView pathList;
+    public ListView nodeList;
+    public CheckBox pathBothWays;
     Algorithm algorithm = new Algorithm();
 
     public void createNodeButtonClick(ActionEvent actionEvent) {
         if(algorithm.findNodeByName(nodeName.getText()) == null) {
             algorithm.createNode(nodeName.getText());
             infoText.setText("Node " + nodeName.getText() + " created.");
+            nodeList.getItems().add(algorithm.findNodeByName(nodeName.getText()));
         }
         else {
             infoText.setText("Node with name " + nodeName.getText() + " already exists.");
@@ -39,9 +41,19 @@ public class Controller {
             infoText.setText("Enter path weight in numbers.");
             return;
         }
+
+        if(algorithm.findNodeByName(pathFrom.getValue().toString()).getConnectedNodeNames().containsKey(pathTo.getValue().toString())){
+            infoText.setText("Path already exists.");
+            return;
+        }
+
+        if(pathBothWays.isSelected()){
+            algorithm.findNodeByName(pathTo.getValue().toString()).addConnection(pathFrom.getValue().toString(), Integer.parseInt(pathWeight.getText()));
+            pathList.getItems().add(pathTo.getValue().toString() + " -> " + pathFrom.getValue().toString() + " | " + pathWeight.getText());
+        }
         algorithm.findNodeByName(pathFrom.getValue().toString()).addConnection(pathTo.getValue().toString(), Integer.parseInt(pathWeight.getText()));
-        algorithm.findNodeByName(pathTo.getValue().toString()).addConnection(pathFrom.getValue().toString(), Integer.parseInt(pathWeight.getText()));
         infoText.setText("Path from " + pathFrom.getValue().toString() + " to " + pathTo.getValue().toString() + " with weight " + pathWeight.getText() + " added.");
+        pathList.getItems().add(pathFrom.getValue().toString() + " -> " + pathTo.getValue().toString() + " | " + pathWeight.getText());
     }
 
     public void tabPaneChange(Event event) {
